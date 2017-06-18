f30286d11fcd90d6fd400fbbe52656382c82076e
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-06-19 01:43:17 +0300

    Add project files

--- collections/Common/FlowControl.cs
+++ collections/Common/FlowControl.cs
@@ -12,10 +12,7 @@ namespace System.Functional
             params (T, Action<T>)[] labels
             ) where T : IEquatable<T>
         {
-            if (@default == null)
-            {
-                throw new ArgumentNullException(nameof(@default));
-            }
+            check(@default);
 
             int i = 0;
             bool notfound = false;
@@ -31,6 +28,62 @@ namespace System.Functional
             else
             {
                 labels[i].Item2?.Invoke(on);
+            }
+        }
+
+        public static void Switch<T>(
+            T on,
+            Action<T> @default,
+            params KeyValuePair<T, Action<T>>[] labels
+            ) where T : IEquatable<T>
+        {
+            check(@default);
+            int i = 0;
+            bool notfound = false;
+            while (i < labels.Length &&
+                  (notfound = !on.Equals(labels[i].Key)))
+            {
+                i++;
+            }
+            if (notfound)
+            {
+                @default?.Invoke(on);
+            }
+            else
+            {
+                labels[i].Value?.Invoke(on);
+            }
+        }
+
+        public static void Switch<T>(
+           T on,
+           Action<T> @default,
+           params Tuple<T, Action<T>>[] labels
+           ) where T : IEquatable<T>
+        {
+            check(@default);
+            int i = 0;
+            bool notfound = false;
+            while (i < labels.Length &&
+                  (notfound = !on.Equals(labels[i].Item1)))
+            {
+                i++;
+            }
+            if (notfound)
+            {
+                @default?.Invoke(on);
+            }
+            else
+            {
+                labels[i].Item2?.Invoke(on);
+            }
+        }
+
+        private static void check<T>(Action<T> @default) where T : IEquatable<T>
+        {
+            if (@default == null)
+            {
+                throw new ArgumentNullException(nameof(@default));
             }
         }
     }
